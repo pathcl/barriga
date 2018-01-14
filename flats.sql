98b8fb4a71f15eff2a2b3db65a82cda1b9feb1d4
Luis San Martin <luis@sanmartin.io>
2018-01-14 19:14:03 -0300

    Update flats.sql

--- flats.sql
+++ flats.sql
@@ -2,7 +2,10 @@ drop table if exists flats;
     create table flats (
     id integer primary key autoincrement,
     timestamp text not null,
-    price NUMERIC not null,
     commune text not null,
+    rooms integer,
+    bathroom integer,
+    size integer,
+    price NUMERIC not null,
     flat text not null
 );
